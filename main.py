b22029f566dcea44bd81086c9f45b5f7d25882f2
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-09-26 10:52:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math;
-x = int(input("Введите значение x: "));
-y = int(input("Введите значение y: "));
-u = (((8 + abs(x-y)**2 + 1 )**(1/3)) / (x**2 + y**2 + 2)) - math.e**abs(x-y);
-print("Результат: ", u);
+import math
+x = int(input("Введите значение x: "))
+y = int(input("Введите значение y: "))
+u = (((8 + abs(x-y)**2 + 1 )**(1/3)) / (x**2 + y**2 + 2)) - math.e**abs(x-y)
+print("Результат: ", u)
